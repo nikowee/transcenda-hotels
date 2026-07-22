dff94aafa9c4bb61601b3839d8fbd5579ee38530
tan-yun-e <tye9793@gmail.com>
2026-07-23 00:24:52 +0800

    removed destination results page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import LandingPage from './pages/LandingPage';
-import DestinationResultsPage from './pages/DestinationResultsPage';
 import HotelDetailPage from './components/HotelDetailsPage';
 
 export default function App() {
@@ -8,7 +7,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/results" element={<DestinationResultsPage />} />
         <Route path="/hotel/:id" element={<HotelDetailPage />} />
       </Routes>
     </BrowserRouter>
