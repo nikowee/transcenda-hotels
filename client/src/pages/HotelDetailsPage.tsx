edf1240844dc5fc455a0af605ec4745a3b50f6e3
tan-yun-e <tye9793@gmail.com>
2026-07-23 09:44:10 +0800

    relocated hoteldetailspage and loading page

--- client/src/pages/HotelDetailsPage.tsx
+++ client/src/pages/HotelDetailsPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import axios from 'axios';
-import LoadingPage from '../components/LoadingPage';
+import LoadingPage from './LoadingPage';
 import type { HotelDetail } from '../types/hotel';
 
 const API_URL = import.meta.env.VITE_API_URL;
